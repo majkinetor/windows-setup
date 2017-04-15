bed292629a212ee2a1496c04eb3b400717680d64
Miodrag Milic <miodrag.milic@gmail.com>
2017-04-15 20:44:53 +0100

    m

--- setup.ps1
+++ setup.ps1
@@ -1,12 +1,12 @@
 ls $PSScriptRoot\src\*.ps1 | % { . $_ }
 if (!(Test-Admin)) {throw "Administrastor privilegies are required"}
 
-Ensure-Chocolatey
-Ensure-ChocolateyPackages
-Restart-Windows -RestartScript
+#Ensure-Chocolatey
+#Ensure-ChocolateyPackages
+#Restart-Windows -RestartScript
 
 Ensure-GitRepositories -Root 'c:\Work' -Repos @(
-    'majkinetor\powershell_profile.d'
-    'majkientor\posh'
-    @{ name='W4RH4WK/Debloat-Windows-10'; path='c:\Work\_'}
+    'majkinetor/powershell_profile.d'
+    'majkinetor/posh'
+    @{ uri='W4RH4WK/Debloat-Windows-10'; path='c:\Work\_'; depth=1; force=$true}
 )
--- src/Ensure-GitRepositories.ps1
+++ src/Ensure-GitRepositories.ps1
@@ -1,7 +1,71 @@
+<#
+.SYNOPSIS
+    Ensure git repositories are cloned.
+
+.DESCRIPTION
+    Ensures repositories given in the list exist locally. Repository can be
+    specified as a string or hashtable with various options.
+#>
 function Ensure-GitRepositories {
     param(
+        # Default clone path if $repo.path is not specified
         [string] $Root = "c:\Source",
+
+        # Prefix to add if repository URI does not contain protocol
+        [string] $Prefix = "https://github.com/",
+
+        # List of repositories to clone, each element is string or hashtable.
+        # Hashtable properties:
+        #   uri   - Repository URI
+        #   path  - Path to clone to
+        #   depth - Git depth parameter
+        #   force - Delete existing directory
         $Repos
     )
 
+    Write-Host "|== Ensure Git Repositories"
+
+    if (!(gcm git.exe -ea 0)) {throw 'Git not found on PATH'}
+
+    git --version
+    Write-Host "Clonning $($repos.Length) git repositories`n"
+    $err=@()
+    $ok=@()
+    foreach ($repo in $Repos) {
+        $r = @{}
+        if ($repo -isnot [hashtable]) { $r.uri = $repo } else { $r = $repo }
+
+        foreach( $protocol in 'http', 'https', 'git', 'ssh') {
+            if ($r.uri.StartsWith($protocol+'://')) { $protocol = 'ok'; break }
+        }
+        if ($protocol -ne 'ok') { $r.uri = $Prefix + $r.uri}
+        $repo_name = $r.uri -split '/' | select -Last 1
+
+        if (!$r.path) {$r.path = $Root}
+        $r.path = Join-Path $r.path $repo_name
+
+        $cmd = "git clone"
+        if ($r.depth) {$cmd += " --depth $($r.depth)"}
+        $cmd += ' {0} {1}' -f $r.uri, $r.path
+        Write-Host $cmd
+
+        if ($r.force) { 
+            Write-Host -ForegroundColor yellow "Force enabled, existing directory will be deleted"
+            rm $r.path -ea 0 -Recurse -Force
+        }
+
+        rm err -ea 0
+        iex $cmd 2> err
+        if (Test-Path err) { 
+            $e = (gc err -Raw) -replace "(?ms)^At line.+?^\s*$" 
+            $e = $e -split "`n" | ? { $_.Trim() } | ? { $_ -notlike "*git : Cloning into*"} | Out-String
+            $e = $e.Trim()
+            rm err -ea 0
+        }
+        if ($e) {Write-Host -ForegroundColor red $e; $err += $r} else {$ok += $r}
+        Write-Host 
+    }
+
+    Write-Host 'Success:' $ok.Length
+    Write-Host 'Failed: ' $err.Length
 }
